3116a19d60474041dfe576fa494494f0a7aace24
Jeff Sutherland <jdsuther@asu.edu>
2017-02-08 13:00:53 +0700

    Formatting fix

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
+var cats = $('.cat');
+var buttons = $('button');
 
-var cats = $(".cat");
-var buttons = $("button");
-
-function hideAllCats(){
-	for (var i=0; i<cats.length; i++){
-		$(cats[i]).hide();
-	}
+function hideAllCats() {
+  for (var i = 0; i < cats.length; i++) {
+    $(cats[i]).hide();
+  }
 }
 
-function bindButtonToCat(idNumber){
-	$("#button"+idNumber).click(function(){
-		hideAllCats();
-		$("#cat"+idNumber).show();
-	})
+function bindButtonToCat(idNumber) {
+  $('#button' + idNumber).click(function () {
+    hideAllCats();
+    $('#cat' + idNumber).show();
+  });
 }
 
-function bindCounterToCat(idNumber){
-	var cat = "#cat"+idNumber
-	$(cat).click(function(){
-		var count = $(cat+" > .counter").text();
-		count = parseInt(count) + 1;
-		$(cat+" > .counter").text(count);
-	})
+function bindCounterToCat(idNumber) {
+  var cat = '#cat' + idNumber;
+  $(cat).click(function () {
+    var count = $(cat + ' > .counter').text();
+    count = parseInt(count) + 1;
+    $(cat + ' > .counter').text(count);
+  });
 }
 
-for (var i=1; i<=buttons.length; i++){
-	bindButtonToCat(i);
+for (var i = 1; i <= buttons.length; i++) {
+  bindButtonToCat(i);
 }
 
-for (var i=1; i<=cats.length; i++){
-	bindCounterToCat(i);
+for (var i = 1; i <= cats.length; i++) {
+  bindCounterToCat(i);
 }
 
 hideAllCats();
-$("#cat1").show();
+$('#cat1').show();
